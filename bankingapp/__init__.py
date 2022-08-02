6f781363ffc3fb9b0ed2a7877c6f90cc2d506eca
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-08-02 13:44:54 +0100

    Update __init__.py

--- bankingapp/__init__.py
+++ bankingapp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
-from config import DevConfig
+from config import ProdConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
@@ -12,7 +12,7 @@ bcrypt = Bcrypt()
 
 def create_app():
     bankingApp = Flask('bankingapp')
-    bankingApp.config.from_object(DevConfig)
+    bankingApp.config.from_object(ProdConfig)
     db.init_app(app=bankingApp)
     migrate.init_app(app=bankingApp, db=db, render_as_batch=True, compare_type=True)
     bcrypt.init_app(app=bankingApp)
