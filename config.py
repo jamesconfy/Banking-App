b8071679493e755a28358ce3037622c521a5b8c5
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2023-01-24 19:03:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,13 @@ PORT = env.get('PORT')
 
 class DevConfig(object):
     SECRET_KEY = env.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{ADDRESS}/{NAME}'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = env.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     JWT_COOKIE_SECURE = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=2)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=10)
     JWT_TOKEN_LOCATION = ["headers"]
     JWT_COOKIE_CSRF_PROTECT = False
+
+class ProdConfig(DevConfig):
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{ADDRESS}/{NAME}'
